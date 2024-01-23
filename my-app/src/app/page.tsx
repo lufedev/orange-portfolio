7e16d99d2827310f799f79751e2e50bedfc056db
quickly <leonardo-programmer@proton.me>
2024-01-23 20:38:51 -0300

    set color preset

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <main></main>
+  return (
+    <div className="bg-red-800">
+      <h1 className="text-color-principal-90">awdadadawda</h1>
+      <h1>adawdawdawdaw</h1>
+      <h1>adawdawdawdaw</h1>
+      <h1>adawdawdawdaw</h1>
+    </div>
+  )
 }
