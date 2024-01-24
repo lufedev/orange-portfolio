c200c06fda8acf6d58e65f36a5a56bada5b2be72
Luiz <felipeluizsantos.k@gmail.com>
2024-01-24 13:05:46 -0300

    Criando branch separada para API

--- my-app/src/app/api/portfolios/route.ts
+++ my-app/src/app/api/portfolios/route.ts
@@ -1,12 +1,26 @@
 import { NextResponse } from "next/server";
 
+
 export async function GET() {
 	return NextResponse.json({
-		hello:"world",
+		status: 200,
+		data: "Pinguei no portfolio",
 		});
+	
 }
+
+
 export async function POST(request: Request){
+	
 	const data = await request.json();
+	const size = Object.keys(data).length;
+	if (size < 1){
+		return NextResponse.json({
+		status: 400,
+		data: "No data received",
+		});
+	}
+		
 	return NextResponse.json({
 		data,
 		});
