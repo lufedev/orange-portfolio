b90bae2f242be80c880a1925bdf14118bf88370c
quickly <leonardo-programmer@proton.me>
2024-01-25 04:52:17 -0300

    button

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,14 @@
+import CustomButton from './components/CustomButton'
+
 export default function Home() {
   return (
-    <div className="bg-red-800">
-      <h1 className="text-color-principal-90">awdadadawda</h1>
-      <h1>adawdawdawdaw</h1>
-      <h1>adawdawdawdaw</h1>
-      <h1>adawdawdawdaw</h1>
+    <div>
+      <CustomButton
+        variant="contained"
+        color="primary"
+        size="large"
+        name="LABEL"
+      />
     </div>
   )
 }
