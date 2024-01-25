aea65ff2d27162f6032e533985b2791176ca6139
Paulo Antonio <dev.pauloantonio785@gmail.com>
2024-01-24 23:35:41 -0300

    Criado o componente Header com material UI

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Header from "./components/header";
+
+
 export default function Home() {
   return (
+   
     <div className="bg-red-800">
+       <Header/>
       <h1 className="text-color-principal-90">awdadadawda</h1>
       <h1>adawdawdawdaw</h1>
       <h1>adawdawdawdaw</h1>
