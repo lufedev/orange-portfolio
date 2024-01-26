d0180bb943e09f140a98c6e0a6c38f3a0f23cd73
Paulo Antonio <dev.pauloantonio785@gmail.com>
2024-01-26 01:56:48 -0300

    Ajutes da estilização do textfield

--- my-app/src/app/components/LoginInput.tsx
+++ my-app/src/app/components/LoginInput.tsx
@@ -1,23 +1,27 @@
 import { ThemeProvider } from '@mui/material/styles'
 import { TextField } from '@mui/material'
 import CustomButton from './CustomButton'
+import { TextFieldTheme } from '../themes/TextField'
 
 export default function LoginInput() {
   return (
     <div className="flex flex-col">
       <h5 className="mb-8 h5 text-color-neutral-110">Faça login com email</h5>
-      <TextField
-        label="Email address"
-        variant="outlined"
-        size="medium"
-        className="mb-4"
-      />
-      <TextField
-        label="Password"
-        variant="outlined"
-        size="medium"
-        className="mb-4"
-      />
+      <ThemeProvider theme={TextFieldTheme}>
+        <TextField
+          label="Email address"
+          variant="outlined"
+          size="medium"
+          className="mb-4"
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          size="medium"
+          className="mb-4"
+        
+        />
+      </ThemeProvider>
       <CustomButton
         theme={true}
         variant="contained"
--- my-app/src/app/themes/TextField.ts
+++ my-app/src/app/themes/TextField.ts
@@ -1,6 +1,19 @@
-import { createTheme } from '@mui/material'
+"use client";
+import { createTheme } from '@mui/material/styles';
+
 
 export const TextFieldTheme = createTheme({
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: '#515255', // Cor padrão do label
+        },
+    
+      },
+    },
+  },
+  
   palette: {
     primary: {
       main: '#FF5522',
