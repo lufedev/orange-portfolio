2ee4de4ad44c239b8dc8ee07ed1905147ec3faf7
quickly <leonardo-programmer@proton.me>
2024-01-31 01:48:38 -0300

    modal mobile first

--- my-app/src/app/components/ModalAddProject.tsx
+++ my-app/src/app/components/ModalAddProject.tsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
 import CustomButton from './CustomButton'
 import { TextFieldTheme } from '../themes/TextField'
+import { DisabledTheme } from '../themes/Button'
 import { ThemeProvider } from '@mui/material/styles'
 import { TextField } from '@mui/material'
 import { styled } from '@mui/material/styles'
@@ -45,36 +46,28 @@ export default function ModalAddProject({
       <Box
         sx={{
           position: 'absolute' as 'absolute',
-          top: '50%',
+          bottom: '0',
           left: '50%',
-          transform: 'translate(-50%, -50%)',
+          transform: 'translate(-50%)',
           width: '80%',
-          height: 'auto',
           bgcolor: 'white',
           border: 'none',
-          padding: '29px 4px',
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
-          gap: 4
+          alignItems: 'end',
+          justifyContent: 'end',
+          marginBottom: '1.5rem'
         }}
       >
-        <Typography
-          id="modal-modal-title"
-          className="text-color-neutral-110 w-4/5 text-left"
-          variant="h5"
-          component="h2"
-        >
-          Adicionar projeto
-        </Typography>
-        <div className="flex flex-col md:flex-row-reverse justify-center items-stretch w-4/5 m-auto text-center gap-3">
-          <div className="w-full md:w-1/2 flex flex-col justify-center items-stretch text-center gap-3">
+        <div className="px-6 w-full">
+          <h5 className="h5 my-4 w-full text-left">Adicionar projeto</h5>
+          <div className="w-full flex flex-col justify-center items-stretch text-center gap-4">
             <ThemeProvider theme={TextFieldTheme}>
               <TextField
                 label="Título"
                 variant="outlined"
                 size="medium"
-                className="mb-4"
+                className=""
                 type="text"
                 value={project?.title || ''}
               />
@@ -82,7 +75,7 @@ export default function ModalAddProject({
                 label="Tags"
                 variant="outlined"
                 size="medium"
-                className="mb-4"
+                className=""
                 type="text"
                 value={project?.tags || ''}
               />
@@ -90,7 +83,7 @@ export default function ModalAddProject({
                 label="Link"
                 variant="outlined"
                 size="medium"
-                className="mb-4"
+                className=""
                 type="text"
                 value={project?.link || ''}
               />
@@ -98,19 +91,19 @@ export default function ModalAddProject({
                 label="Descrição"
                 variant="outlined"
                 size="medium"
-                className="mb-4"
+                className=""
                 type="text"
                 multiline
-                rows={4}
+                rows={3}
                 value={project?.description || ''}
               />
             </ThemeProvider>
           </div>
-          <div className="w-full md:w-1/2 flex flex-col justify-center items-center gap-8">
-            <h4 className="subtitle-1 text-color-neutral-100">
+          <div className="my-4">
+            <p className="subtitle-1 text-color-neutral-110 text-left w-full mb-4">
               Selecione o conteúdo que você deseja fazer upload
-            </h4>
-            <div className="rounded w-full md:w-[100%] h-[100%] text-lowercase w-327 h-330 flex flex-col space-y-4 justify-center items-center">
+            </p>
+            <div className="w-full h-[304px]">
               {isUrlImage ? (
                 <Image
                   src={project?.urlImage}
@@ -120,51 +113,51 @@ export default function ModalAddProject({
                   className="mr-6 mb-[16px]"
                 />
               ) : (
-                <Button
-                  component="label"
-                  disableElevation
-                  variant="contained"
-                  className="w-[100%] h-[80%] bg-color-neutral-70 hover:bg-color-neutral-70 flex flex-col justify-center items-center text-color-neutral-120 font-normal normal-case"
-                >
-                  <PhotoLibraryIcon className="text-5xl" />
-                  <p className="w-[80%]">
-                    Compartilhe seu talento com milhares de pessoas
-                  </p>
-                  <VisuallyHiddenInput type="file" />
-                </Button>
+                <ThemeProvider theme={DisabledTheme}>
+                  <Button
+                    component="label"
+                    disableElevation
+                    variant="contained"
+                    className="h-full px-2 py-0 flex flex-col text-color-neutral-120 leading-[0.875rem] tracking-[0.01563rem] font-normal normal-case"
+                  >
+                    <PhotoLibraryIcon className="text-5xl" />
+                    <p className="w-full text-left flex justify-center mt-4">
+                      Compartilhe seu talento com milhares de pessoas
+                    </p>
+                    <VisuallyHiddenInput type="file" />
+                  </Button>
+                </ThemeProvider>
               )}
             </div>
           </div>
-        </div>
-
-        <div className="flex flex-col justify-center items-start gap-8 w-4/5">
-          <a
-            href="https://www.youtube.com/"
-            className="subtitle-1 text-color-neutral-100 !no-underline"
-          >
-            Visualizar publicação
-          </a>
-
-          <div className="flex justify-center items-start gap-4">
-            <CustomButton
-              theme="ContainedTheme"
-              variant="contained"
-              color="primary"
-              size="large"
-              disabled={false}
-              name="SALVAR"
-              loading={false}
-            />
-            <CustomButton
-              theme="disabled"
-              variant="contained"
-              color="primary"
-              size="large"
-              disabled={false}
-              name="CANCELAR"
-              loading={false}
-              onClick={handleToggle}
-            />
+          <div className="flex flex-col w-full items-start">
+            <a
+              href="https://www.youtube.com/"
+              className="subtitle-1 text-color-neutral-100 !no-underline mb-4"
+            >
+              Visualizar publicação
+            </a>
+            <div className="flex mb-10 gap-4">
+              <CustomButton
+                theme="ContainedTheme"
+                variant="contained"
+                color="primary"
+                size="large"
+                disabled={false}
+                name="SALVAR"
+                loading={false}
+              />
+              <CustomButton
+                theme="disabled"
+                variant="contained"
+                color="primary"
+                size="large"
+                disabled={false}
+                name="CANCELAR"
+                loading={false}
+                onClick={handleToggle}
+              />
+            </div>
           </div>
         </div>
       </Box>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -47,11 +47,7 @@ export default function Home() {
       <Header user={user} />
       <div className="flex flex-col items-center justify-start mt-14 mx-6 gap-10">
         <CardProfile user={user} onClick={openModal} />
-        <ModalAddProject
-          project={project}
-          states={modalOpen}
-          onClose={closeModal}
-        />
+        <ModalAddProject states={modalOpen} onClose={closeModal} />
         <div className="w-full mb-6">
           <h4 className="h6 text-color-neutral-130 mb-4">Meus projetos</h4>
           <ThemeProvider theme={TextFieldTheme}>
