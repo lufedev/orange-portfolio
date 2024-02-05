595e7c5ac4fdd47a68bb1257bc5048912c70ad42
Luiz <felipeluizsantos.k@gmail.com>
2024-02-05 14:52:06 -0300

    Configuring postcss

--- my-app/postcss.config.js
+++ my-app/postcss.config.js
@@ -1,20 +1,11 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    [
-      {
-        allowDuplicates: false,
-      },
-    ],
-    [
-      {
-        content: [
-          './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-          './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
-          './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      },
-    ],
-    'autoprefixer',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
