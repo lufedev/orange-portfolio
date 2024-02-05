b21392db1cf304a674406029de20e11cf6ab7515
Luiz <felipeluizsantos.k@gmail.com>
2024-02-05 16:32:07 -0300

    Testando VERCEL_URL

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -10,6 +10,7 @@ export default async function Dashboard() {
   if (!session) {
     redirect('/login')
   }
+  console.log(process.env.VERCEL_URL)
   //@ts-expect-error  Ignorando Props Obrigatórias
   return <Home sessionData={user} />
 }
