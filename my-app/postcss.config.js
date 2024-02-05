23baec472fe4df3f7ae23a93740d38a252374a30
Luiz <felipeluizsantos.k@gmail.com>
2024-02-05 14:47:28 -0300

    Configuring module exports

--- my-app/postcss.config.js
+++ my-app/postcss.config.js
@@ -1,6 +1,24 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    'postcss-preset-env',
+    [
+      'postcss-normalize',
+      {
+        allowDuplicates: false,
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+          './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
+          './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ],
+    'autoprefixer',
+  ],
 };
