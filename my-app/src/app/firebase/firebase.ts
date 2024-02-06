7c26769c2dacf7c8a60f77855e0469bef33bd680
Luiz <felipeluizsantos.k@gmail.com>
2024-02-05 22:59:46 -0300

    Adding another firebase

--- my-app/src/app/firebase/firebase.ts
+++ my-app/src/app/firebase/firebase.ts
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBT3eaadZKwSw4q_99gq9msoxNFCJc9Sc4',
-  authDomain: 'orange-portfolio-b7f67.firebaseapp.com',
-  projectId: 'orange-portfolio-b7f67',
-  storageBucket: 'orange-portfolio-b7f67.appspot.com',
-  messagingSenderId: '158939880663',
-  appId: '1:158939880663:web:249b949421dbc4d234a492',
-  measurementId: 'G-TF6TH73JJH'
+  apiKey: 'AIzaSyCnv9JZbLmWUjWR3oR358c5qiX8Zgn5A-c',
+  authDomain: 'orange-portfolio-2.firebaseapp.com',
+  projectId: 'orange-portfolio-2',
+  storageBucket: 'orange-portfolio-2.appspot.com',
+  messagingSenderId: '213016075286',
+  appId: '1:213016075286:web:3423757a8aae9a5c4c6937',
+  measurementId: 'G-S890V52KZY'
 }
 
 if (!firebase.apps.length) {
